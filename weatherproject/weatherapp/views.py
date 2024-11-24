6381ce9098167bbe6ac9d0c86b0b3ae05e25b2ca
Ashley-Wilson1 <ashley03wilson@gmail.com>
2024-11-24 23:55:01 +0000

    Added weather api

--- weatherproject/weatherapp/views.py
+++ weatherproject/weatherapp/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render,HttpResponse
-
+from django.shortcuts import render
+import requests
+import datetime
 # Create your views here.
-def home(reequest):
-    return HttpResponse("hey this is my Django server ")
+def home(request):
+
+    if 'city' in request.POST:
+        city = request.POST['city']
+    else:
+        city = 'sheffield'
+    url =f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid=696cd71a9679e055fc8039459ade9cbc'
+    PARAMS = {'units':'metric'}
+
+    data = request.get(url,PARAMS).json()
+    description = data['weather'][0]['description']
+    icon = data['weather'][0]['icon']
+    temp = data['main']['temp']
+
+    day = datetime.date.today
+
+    return render(request,'weatherapp/index.html',{'description':description,'icon':icon,'temp':temp,'day':day})
