7ce077d45c480cf265375d4eec3f733f2d93505f
Ashley-Wilson1 <ashley03wilson@gmail.com>
2024-11-25 01:03:28 +0000

    Html added for index and fixes to views

--- weatherproject/weatherapp/views.py
+++ weatherproject/weatherapp/views.py
@@ -11,11 +11,11 @@ def home(request):
     url =f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid=696cd71a9679e055fc8039459ade9cbc'
     PARAMS = {'units':'metric'}
 
-    data = request.get(url,PARAMS).json()
+    data = requests.get(url,PARAMS).json()
     description = data['weather'][0]['description']
     icon = data['weather'][0]['icon']
     temp = data['main']['temp']
 
     day = datetime.date.today
 
-    return render(request,'weatherapp/index.html',{'description':description,'icon':icon,'temp':temp,'day':day})
+    return render(request,'index.html',{'description':description,'icon':icon,'temp':temp,'day':day})
