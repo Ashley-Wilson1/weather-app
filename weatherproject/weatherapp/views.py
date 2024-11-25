e1b57cad6a57549dd4df5a60d11cf07b98d50c26
Ashley-Wilson1 <ashley03wilson@gmail.com>
2024-11-25 15:30:15 +0000

    Handled invalid city names, fixed bug passing city

--- weatherproject/weatherapp/views.py
+++ weatherproject/weatherapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.contrib import messages
 import requests
 import datetime
+
 # Create your views here.
 def home(request):
 
@@ -12,11 +14,19 @@ def home(request):
     url =f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid=696cd71a9679e055fc8039459ade9cbc'
     PARAMS = {'units':'metric'}
 
-    data = requests.get(url,PARAMS).json()
-    description = data['weather'][0]['description']
-    icon = data['weather'][0]['icon']
-    temp = data['main']['temp']
+    try:
+        data = requests.get(url,PARAMS).json()
 
-    day = datetime.date.today
+        description = data['weather'][0]['description']
+        icon = data['weather'][0]['icon']
+        temp = data['main']['temp']
 
-    return render(request,'index.html',{'description':description,'city':city,'icon':icon,'temp':temp,'day':day})
+        day = datetime.date.today
+        
+        return render(request,'index.html',{'description':description,'icon':icon,'temp':temp,'day':day,'city':city, 'exception_occurred':False })
+    except:
+        exception_occurred = True
+        messages.error(request,'entered data is not availible to API')
+        day = datetime.date.today
+
+        return render(request,'index.html',{'description':'clear sky', 'icon':'01d'  ,'temp':25 , 'day':day , 'city':'sheffield' , 'exception_occurred':exception_occurred })
